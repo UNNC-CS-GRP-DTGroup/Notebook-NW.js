8cade6fd09a52a4dcd146834e1eb834f2595a7f6
PAN001 <PAN.Atopos@outlook.com>
2016-02-07 18:02:07 +0800

    upload all files so far

---
+++ public/js/editor.js
@@ -0,0 +1,22 @@
+function initEditor() {
+    tinymce.init({
+      selector: '#editor h1',
+      inline: true,
+      toolbar: 'undo redo',
+      menubar: false
+    });
+
+    tinymce.init({
+      selector: '#editor div',
+      inline: true,
+      plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table contextmenu'
+//                                      去除掉“paste”，DD时正常（不会多余复制）
+      ],
+      toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
+    });
+}
+
+initEditor();
