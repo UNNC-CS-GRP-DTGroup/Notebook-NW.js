9a3856d39429c3111b6791e4b7e22ca8652c9858
PAN001 <PAN.Atopos@outlook.com>
2016-03-04 09:10:56 +0800

    changes to test files

--- myapp/expressServer.js
+++ myapp/expressServer.js
@@ -3,7 +3,7 @@ var http = require('http');
 var bodyParser = require("body-parser");
 
 var app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.text());
 
 console.log("server starts");
 
@@ -11,6 +11,7 @@ app.use(function(req, res, next) {
 	console.log("run");
 	res.header('Access-Control-Allow-Origin', '*'); 
 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+//    res.header( 'Content-Type' ,'application/x-www-form-urlencoded; charset=UTF-8');
 	//res.header('Access-Control-Allow-Headers', 'X-Requested_With'); 
 	// console.log(req);
 	next();
@@ -20,7 +21,9 @@ app.use(function(req, res, next) {
 app.post("*", function(req, res) {
 	// res.header('Access-Control-Allow-Origin', '*'); // implementation of CORS
 	console.log("get data");
-	console.log(req.body);
+	console.log(req);
+    console.log("Body is: " + req.body);
+    console.log("The UserId is: " + JSON.parse(req.body).UserId);
 });
 
 app.listen(8000);
