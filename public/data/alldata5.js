370500b20f04a59baad7764d3e06936648de42b5
PAN001 <PAN.Atopos@outlook.com>
2016-03-12 21:52:46 +0800

    fix the bug when uploading. Start to replace url with baseUrl

--- public/data/alldata5.js
+++ public/data/alldata5.js
@@ -189,89 +189,155 @@ function sleep(miliseconds) {
 }
 
 //加载用户名：
-var email = localStorage.email;
-$("#username").html(email);
-var UserId = "56a82c7bab64417776002a5c";
-var isConnected = true;
+var Email = localStorage.email;
+var UserId = localStorage.UserId;
+var Password = localStorage.password;
+$("#username").html(Email);
+//var UserId = "56a82c7bab64417776002a5c";
+//var isConnected = true;
+var isConnected = navigator.onLine;
+
+UserInfo = {"UserId": UserId,"Email": Email,"Verified": true, "Username":Email, "UsernameRaw":"","CreatedTime":"0001-01-01T00:00:00Z","Logo":"http://leanote.com/images/blog/default_avatar.png","Theme":"","NotebookWidth":0,"NoteListWidth":0,"MdEditorWidth":0,"LeftIsMin":false,"ThirdUserId":"","ThirdUsername":"","ThirdType":0,"FromUserId":"","NoteCnt":0,"Usn":10,"FullSyncBefore":"0001-01-01T00:00:00Z","BlogUrl":"","PostUrl":"", "Password": Password};
 
 
-//同步策略二：以server为优
-if(isConnected) { // 如果有网，直接从server拿
-    $.ajax({
-        type: 'GET', // added,
-        url: 'http://cs-linux.nottingham.edu.cn:8000/getAll',
-        data: "UserId=" + UserId ,
-    //                dataType: "json",
-    //                contentType: "application/json; charset=UTF-8",
-        //dataType: 'jsonp' - removed
-        //jsonpCallback: 'callback' - removed
-        contentType: "text/plain", 
-        async:false, 
-        success: function (data) {
-            var parsedData = jQuery.parseJSON(data);
-            allAppData = parsedData;    
-            console.log(allAppData.UserInfo.UserId);
 
-            console.log("allAppData loaded from server");
+////同步策略二：以server为优
+//if(isConnected) { // 如果有网，直接从server拿
+//    $.ajax({
+//        type: 'GET', // added,
+//        url: 'http://cs-linux.nottingham.edu.cn:8000/getAll',
+//        data: "UserId=" + UserId ,
+//    //                dataType: "json",
+//    //                contentType: "application/json; charset=UTF-8",
+//        //dataType: 'jsonp' - removed
+//        //jsonpCallback: 'callback' - removed
+//        contentType: "text/plain", 
+//        async:false, 
+//        success: function (data) {
+//            var parsedData = jQuery.parseJSON(data);
+//            if(parsedData.status && parsedData.status == "fail") { // 服务端没有数据：使用初始数据
+//                console.log("not found data");
+//                //使用初始数据
+//                //initial data    
+//                allAppData.UserInfo = UserInfo;
+//                allAppData.notebooks = notebooks;
+//                allAppData.shareNotebooks = shareNotebooks;
+//                allAppData.sharedUserInfos = sharedUserInfos;
+//                allAppData.notes = notes;
+//                allAppData.latestNotes = latestNotes;
+//                allAppData.tagsJson = tagsJson;
+//                allAppData.trackingLog = trackingLog;
+//                console.log("using initial data");
+//                console.log("call initPage()");
+//                initPage();
+//                localforage.setItem("allAppData", allAppData, function(err, value) {
+//                    console.log("allAppData saved");
+//                });
+//                return;
+//            } 
+//            allAppData = parsedData;    
+//            console.log(allAppData.UserInfo.UserId);
+//
+//            console.log("allAppData loaded from server");
+//
+//            UserInfo = allAppData.UserInfo;
+//            console.log("UserInfo updated from server");
+//            notebooks = allAppData.notebooks;
+//            console.log("notebooks updated from server");
+//            shareNotebooks = allAppData.shareNotebooks;
+//            console.log("shareNotebooks updated from server");
+//            sharedUserInfos = allAppData.sharedUserInfos;
+//            console.log("sharedUserInfos updated from server");
+//            notes = allAppData.notes;
+//            console.log("notes updated from server");
+//            latestNotes = allAppData.latestNotes;
+//            console.log("latestNotes updated from server");
+//            tagsJson = allAppData.tagsJson;
+//            console.log("tagsJson updated from server");
+//            trackingLog = allAppData.trackingLog;
+//            console.log("trackingLog updated from server");
+//            console.log("call initPage()");
+//            initPage();
+//            localforage.setItem("allAppData", allAppData, function(err, value) {
+//                console.log("allAppData saved");
+//            });
+//        },
+//        error: function (xhr, status, error) {
+//            console.log('Error: ' + error.message);
+//            $('#lblResponse').html('Error connecting to the server.');
+//        }
+//    });
+//}
+//else {// 如果没网：1. 从本地拿；2. 本地没有使用初始数据
+//    localforage.getItem('allAppData', function(err, value) {
+//        console.log("check");
+//        if(value != null) { //如果本地有数据，使用本地的
+//                allAppData = value;
+//                console.log("allAppData loaded from local");
+//
+//                UserInfo = allAppData.UserInfo;
+//                console.log("UserInfo updated from local");
+//                notebooks = allAppData.notebooks;
+//                console.log("notebooks updated from local");
+//                shareNotebooks = allAppData.shareNotebooks;
+//                console.log("shareNotebooks updated from local");
+//                sharedUserInfos = allAppData.sharedUserInfos;
+//                console.log("sharedUserInfos updated from local");
+//                notes = allAppData.notes;
+//                console.log("notes updated from local");
+//                latestNotes = allAppData.latestNotes;
+//                console.log("latestNotes updated from local");
+//                tagsJson = allAppData.tagsJson;
+//                console.log("tagsJson updated from local");
+//                trackingLog = allAppData.trackingLog;
+//                console.log("trackingLog updated from local");
+//                console.log("call initPage() from local");
+//                initPage();
+//                localforage.setItem("allAppData", allAppData, function(err, value) {
+//                    console.log("allAppData saved");
+//                });
+//        }
+//        else { //使用初始数据
+//            //initial data    
+//            allAppData.UserInfo = UserInfo;
+//            allAppData.notebooks = notebooks;
+//            allAppData.shareNotebooks = shareNotebooks;
+//            allAppData.sharedUserInfos = sharedUserInfos;
+//            allAppData.notes = notes;
+//            allAppData.latestNotes = latestNotes;
+//            allAppData.tagsJson = tagsJson;
+//            allAppData.trackingLog = trackingLog;
+//            console.log("using initial data");
+//            console.log("call initPage()");
+//            initPage();
+//            localforage.setItem("allAppData", allAppData, function(err, value) {
+//                console.log("allAppData saved");
+//            });
+//        }
+//    });
+//}
 
-            UserInfo = allAppData.UserInfo;
-            console.log("UserInfo updated from server");
-            notebooks = allAppData.notebooks;
-            console.log("notebooks updated from server");
-            shareNotebooks = allAppData.shareNotebooks;
-            console.log("shareNotebooks updated from server");
-            sharedUserInfos = allAppData.sharedUserInfos;
-            console.log("sharedUserInfos updated from server");
-            notes = allAppData.notes;
-            console.log("notes updated from server");
-            latestNotes = allAppData.latestNotes;
-            console.log("latestNotes updated from server");
-            tagsJson = allAppData.tagsJson;
-            console.log("tagsJson updated from server");
-            trackingLog = allAppData.trackingLog;
-            console.log("trackingLog updated from server");
-            console.log("call initPage()");
-            initPage();
-            localforage.setItem("allAppData", allAppData, function(err, value) {
-                console.log("allAppData saved");
-            });
-        },
-        error: function (xhr, status, error) {
-            console.log('Error: ' + error.message);
-            $('#lblResponse').html('Error connecting to the server.');
-        }
-    });
-}
-else {// 如果没网：1. 从本地拿；2. 本地没有使用初始数据
-    localforage.getItem('allAppData', function(err, value) {
-        console.log("check");
-        if(value != null) { //如果本地有数据，使用本地的
-                allAppData = value;
-                console.log("allAppData loaded from local");
 
-                UserInfo = allAppData.UserInfo;
-                console.log("UserInfo updated from local");
-                notebooks = allAppData.notebooks;
-                console.log("notebooks updated from local");
-                shareNotebooks = allAppData.shareNotebooks;
-                console.log("shareNotebooks updated from local");
-                sharedUserInfos = allAppData.sharedUserInfos;
-                console.log("sharedUserInfos updated from local");
-                notes = allAppData.notes;
-                console.log("notes updated from local");
-                latestNotes = allAppData.latestNotes;
-                console.log("latestNotes updated from local");
-                tagsJson = allAppData.tagsJson;
-                console.log("tagsJson updated from local");
-                trackingLog = allAppData.trackingLog;
-                console.log("trackingLog updated from local");
-                console.log("call initPage() from local");
-                initPage();
-                localforage.setItem("allAppData", allAppData, function(err, value) {
-                    console.log("allAppData saved");
-                });
-        }
-        else { //使用初始数据
+
+
+
+
+//同步策略三
+$.ajax({
+    type: 'GET', // added,
+    url: 'http://cs-linux.nottingham.edu.cn:8000/getAll',
+    data: "UserId=" + UserId ,
+//                dataType: "json",
+//                contentType: "application/json; charset=UTF-8",
+    //dataType: 'jsonp' - removed
+    //jsonpCallback: 'callback' - removed
+    contentType: "text/plain", 
+    async:false, 
+    success: function (data) {
+        var parsedData = jQuery.parseJSON(data);
+        if(parsedData.status && parsedData.status == "fail") { // 服务端没有数据：使用初始数据
+            console.log("not found data");
+            //使用初始数据
             //initial data    
             allAppData.UserInfo = UserInfo;
             allAppData.notebooks = notebooks;
@@ -287,10 +353,86 @@ else {// 如果没网：1. 从本地拿；2. 本地没有使用初始数据
             localforage.setItem("allAppData", allAppData, function(err, value) {
                 console.log("allAppData saved");
             });
-        }
-    });
-}
+            return;
+        } 
+        allAppData = parsedData;    
+        console.log(allAppData.UserInfo.UserId);
 
+        console.log("allAppData loaded from server");
+
+        UserInfo = allAppData.UserInfo;
+        console.log("UserInfo updated from server");
+        notebooks = allAppData.notebooks;
+        console.log("notebooks updated from server");
+        shareNotebooks = allAppData.shareNotebooks;
+        console.log("shareNotebooks updated from server");
+        sharedUserInfos = allAppData.sharedUserInfos;
+        console.log("sharedUserInfos updated from server");
+        notes = allAppData.notes;
+        console.log("notes updated from server");
+        latestNotes = allAppData.latestNotes;
+        console.log("latestNotes updated from server");
+        tagsJson = allAppData.tagsJson;
+        console.log("tagsJson updated from server");
+        trackingLog = allAppData.trackingLog;
+        console.log("trackingLog updated from server");
+        console.log("call initPage()");
+        initPage();
+        localforage.setItem("allAppData", allAppData, function(err, value) {
+            console.log("allAppData saved");
+        });
+    },
+    error: function (xhr, status, error) {
+        console.log('Error: ' + error.message);
+        
+        localforage.getItem('allAppData', function(err, value) {
+            console.log("check");
+            if(value != null) { //如果本地有数据，使用本地的
+                    allAppData = value;
+                    console.log("allAppData loaded from local");
+
+                    UserInfo = allAppData.UserInfo;
+                    console.log("UserInfo updated from local");
+                    notebooks = allAppData.notebooks;
+                    console.log("notebooks updated from local");
+                    shareNotebooks = allAppData.shareNotebooks;
+                    console.log("shareNotebooks updated from local");
+                    sharedUserInfos = allAppData.sharedUserInfos;
+                    console.log("sharedUserInfos updated from local");
+                    notes = allAppData.notes;
+                    console.log("notes updated from local");
+                    latestNotes = allAppData.latestNotes;
+                    console.log("latestNotes updated from local");
+                    tagsJson = allAppData.tagsJson;
+                    console.log("tagsJson updated from local");
+                    trackingLog = allAppData.trackingLog;
+                    console.log("trackingLog updated from local");
+                    console.log("call initPage() from local");
+                    initPage();
+                    localforage.setItem("allAppData", allAppData, function(err, value) {
+                        console.log("allAppData saved");
+                    });
+            }
+            else { //使用初始数据
+                //initial data    
+                allAppData.UserInfo = UserInfo;
+                allAppData.notebooks = notebooks;
+                allAppData.shareNotebooks = shareNotebooks;
+                allAppData.sharedUserInfos = sharedUserInfos;
+                allAppData.notes = notes;
+                allAppData.latestNotes = latestNotes;
+                allAppData.tagsJson = tagsJson;
+                allAppData.trackingLog = trackingLog;
+                console.log("using initial data");
+                console.log("call initPage()");
+                initPage();
+                localforage.setItem("allAppData", allAppData, function(err, value) {
+                    console.log("allAppData saved");
+                });
+            }
+        });
+    }
+});
 
 
 // 同步策略一：以本地为优
